c69624458fb5c735e636c6a8068841de464c8e19
kaedeair <34467508+kaedeair@users.noreply.github.com>
2024-06-13 16:20:33 +0800

    feat(project): [i18n] add translation zh-cn

---
+++ src/i18n/zh-cn.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../service/FileService.cpp" line="34"/>
+        <source>Dir %1 not found!</source>
+        <translation>未找到文件夹 %1 !</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="32"/>
+        <source>%1 does not exist</source>
+        <translation>%1 不存在</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="34"/>
+        <source>%1 is not writable</source>
+        <translation>%1 不可写</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="45"/>
+        <source>File %1 already exists</source>
+        <translation>文件 %1 已存在</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="47"/>
+        <source>Config %1 has no target</source>
+        <translation>配置 %1 没有目标</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="49"/>
+        <source>Number of target in config %1 is out of range</source>
+        <translation>配置 %1 中断目标数量超出了范围</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="59"/>
+        <source>Target %1 in config %2 is invalid</source>
+        <translation>配置 %2 中的目标 %1 参数无效</translation>
+    </message>
+    <message>
+        <location filename="../service/WarpService.cpp" line="76"/>
+        <source>Hardlink %1 create failed for %2. fallback now...</source>
+        <translation>由于 %2硬链接 %1 创建失败,采用备用方案...</translation>
+    </message>
+    <message>
+        <location filename="../controller/WarpController.hpp" line="53"/>
+        <location filename="../controller/WarpController.hpp" line="55"/>
+        <source>Field savePath can not be empty</source>
+        <translation>savePath字段不能为空</translation>
+    </message>
+    <message>
+        <location filename="../controller/WarpController.hpp" line="57"/>
+        <source>Field warpConfig can not be empty</source>
+        <translation>warpConfig字段不能为空</translation>
+    </message>
+    <message>
+        <location filename="../controller/FileController.hpp" line="56"/>
+        <source>Field dirPath can not be Empty</source>
+        <translation>dirPath字段不能为空</translation>
+    </message>
+    <message>
+        <location filename="../App.cpp" line="48"/>
+        <source>Running on port %1...</source>
+        <translation>运行于端口%1...</translation>
+    </message>
+</context>
+</TS>
